81e642dae0a0fb6c76d1387db5104e28abb59eeb
A6BZHZZ <jhc@gmail.com>
2017-10-10 16:28:21 -0500

    currency converter upload

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var exchangeRates = require('../model/currencyDB');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET convert page. */
+router.get('/convert', function(req, res, next) {
+    var query = req.query;
+    console.log(exchangeRates);
+
+    var dollars = req.query.dollars;
+    var toCurrency = req.query.to_currency;
+
+    var converted = dollars * exchangeRates[toCurrency];
+    res.render('results', {
+      dollars: dollars,
+        toCurrency: toCurrency,
+        converted: converted});
+});
+
+
+/* GET about page. */
+router.get('/about', function(req, res, next){
+  res.render('about', {name: "my awesome site"});
+});
+
+module.exports = router;
